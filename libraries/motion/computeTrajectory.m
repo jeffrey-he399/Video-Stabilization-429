16955b44abf1bb0a05d4ef888f4ab262fc960be0
jeffrey-he399 <jeffrey.he0115@gmail.com>
2026-07-28 17:09:43 -0700

    Refactor computeTrajectory for Windows compatibility
    Updated computeTrajectory function for better performance and compatibility.

--- libraries/motion/computeTrajectory.m
+++ libraries/motion/computeTrajectory.m
@@ -1,14 +1,21 @@
-% by Mateo Noriega
+% by Mateo Noriega & Jeffrey He
 % Input: vid_obj - VideoReader object with M frames
 % Output: trajectory - M x 3 matrix where each frame contains the cumulative movement 
 %                      data of the camera relative to the start of the video [dx, dy, dθ (in degrees)]
-function trajectory = computeTrajectory(vid_obj)
-  M = vid_obj.NumFrames;
-  trajectory = zeros(M, 3);
+%
+% Windows compatible version
 
-  for i = 1:(M-1)
-    frameA_rgb = extractFrame(vid_obj, i);
-    frameB_rgb = extractFrame(vid_obj, i+1);
+function trajectory = computeTrajectory(vid_obj)
+  vid_obj = VideoReader(fullfile(vid_obj.Path, vid_obj.Name)); % Use a fresh reader in case the original reader is at the end of the file
+
+  estimated_frames = max(1, ceil(vid_obj.Duration * vid_obj.FrameRate));
+  trajectory = zeros(estimated_frames, 3);
+
+  frameA_rgb = readFrame(vid_obj);
+  i = 1;
+
+  while hasFrame(vid_obj)
+    frameB_rgb = readFrame(vid_obj);
 
     transform = estimateMotion(frameA_rgb, frameB_rgb);
 
@@ -17,5 +24,9 @@ function trajectory = computeTrajectory(vid_obj)
     dAngle = transform.RotationAngle;
 
     trajectory(i+1, :) = trajectory(i, :) + [dx, dy, dAngle]; % Accumulate in i+1
+    frameA_rgb = frameB_rgb;
+    i = i + 1;
   end
+
+  trajectory = trajectory(1:i, :);
 end
